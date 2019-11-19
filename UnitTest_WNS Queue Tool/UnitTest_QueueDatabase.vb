f689cfae907b4159f9fc62fbf29f3fefa58c1702
KurtLouis AquinoWNS <kurtlouis.aquino@one-line.com>
2019-11-19 10:21:16 +0000

    ParallelUpload_AssignQueueItem
    11/19/2019 - kurt.
    please QA

--- UnitTest_WNS Queue Tool/UnitTest_QueueDatabase.vb
+++ UnitTest_WNS Queue Tool/UnitTest_QueueDatabase.vb
@@ -20,6 +20,118 @@ Imports WNSQueueTool
         SUT = New QueueDatabase(mConfig.ConnectionString, mConfig.DeadLockRetryCount)
         SUT1 = New QueueDatabase(mConfig1.ConnectionString, mConfig1.DeadLockRetryCount)
     End Sub
+    <TestMethod()> Public Sub ParallelUpload_StatusCode_DtTable()
+        Dim StatusCode As String
+        Dim StatusDesc As String
+        Dim UseThisStr As String
+        Dim rowsAffected As Long
+
+        StatusCode = InputBox("Status Code", "Insert Transaction  Status code that will be added in DB.")
+        StatusDesc = InputBox("Status Description", "Insert the small description.")
+        SUT1.Connect()
+        'insert
+        UseThisStr = mConfig1.Queries(9).Replace("[StatusCode]", StatusCode)
+        UseThisStr = UseThisStr.Replace("[StatusDesc]", StatusDesc)
+        rowsAffected = SUT1.ExecuteNonQuery(UseThisStr)
+        Assert.IsTrue(rowsAffected = 1)
+
+        'update Status Code
+        UseThisStr = mConfig1.Queries(10).Replace("[StatusCode]", StatusCode)
+        rowsAffected = SUT1.ExecuteNonQuery(UseThisStr.Replace("[Replacer]", "K"))
+        Assert.IsTrue(rowsAffected = 1)
+
+        'update Status Desc
+        UseThisStr = mConfig1.Queries(11).Replace("[StatusDesc]", StatusDesc)
+        rowsAffected = SUT1.ExecuteNonQuery(UseThisStr.Replace("[Replacer]", "test"))
+        Assert.IsTrue(rowsAffected = 1)
+
+        'delete
+        UseThisStr = mConfig1.Queries(12).Replace("[StatusCode]", "K")
+        rowsAffected = SUT1.ExecuteNonQuery(UseThisStr)
+        Assert.IsTrue(rowsAffected = 1)
+        SUT1.Disconnect()
+    End Sub
+    <TestMethod()> Public Sub ParallelUpload_AssignQueue()
+        Dim rowsAffected As Long
+        Dim AssignToUID As String
+        Dim StartStatusCode As String
+        Dim EndStatusCode As String
+        Dim Remarks As String
+        Dim UseThisStr As String
+        SUT1.Connect()
+        AssignToUID = InputBox("assign to", "uid")
+        StartStatusCode = InputBox("Status", "StatusCode")
+        EndStatusCode = InputBox("EndCode", "End Code")
+        Remarks = InputBox("Remarks", "Remarks")
+
+        'insert
+        UseThisStr = mConfig1.Queries(13)
+        If (StartStatusCode = "") Then
+            MsgBox("Start Code needs to be non-null")
+            Exit Sub
+        Else
+            UseThisStr = UseThisStr.Replace("[StartStatusCode]", StartStatusCode)
+        End If
+        If (EndStatusCode = "") Then
+            UseThisStr = UseThisStr.Replace(",EndStatusCode", "")
+            UseThisStr = UseThisStr.Replace(",'[EndStatusCode]'", "")
+        Else
+            UseThisStr = UseThisStr.Replace("[EndStatusCode]", EndStatusCode)
+        End If
+        If (AssignToUID = "") Then
+            MsgBox("Assign ID cannot be null")
+            Exit Sub
+        Else
+            UseThisStr = UseThisStr.Replace("[AssignToUID]", AssignToUID)
+        End If
+        If (Remarks = "") Then
+            UseThisStr = UseThisStr.Replace(",Remarks", "")
+            UseThisStr = UseThisStr.Replace(",'[Remarks]'", "")
+        Else
+            UseThisStr = UseThisStr.Replace("[Remarks]", Remarks)
+        End If
+        UseThisStr = UseThisStr.Replace("[ComputerName]", Environment.MachineName)
+        UseThisStr = UseThisStr.Replace("[AssignByUID]", Environment.UserName)
+        rowsAffected = SUT1.ExecuteNonQuery(UseThisStr)
+        Assert.IsTrue(rowsAffected = 1)
+
+        'update
+        UseThisStr = mConfig1.Queries(14)
+        If (StartStatusCode = "") Then
+            MsgBox("Start Code needs to be non-null")
+            Exit Sub
+        Else
+            UseThisStr = UseThisStr.Replace("[StartStatusCode]", StartStatusCode)
+        End If
+        If (EndStatusCode = "") Then
+            UseThisStr = UseThisStr.Replace(",EndStatusCode", "")
+            UseThisStr = UseThisStr.Replace(",'[EndStatusCode]'", "")
+        Else
+            UseThisStr = UseThisStr.Replace("[EndStatusCode]", EndStatusCode)
+        End If
+        If (AssignToUID = "") Then
+            MsgBox("Assign ID cannot be null")
+            Exit Sub
+        Else
+            UseThisStr = UseThisStr.Replace("[AssignToUID]", AssignToUID)
+        End If
+        If (Remarks = "") Then
+            UseThisStr = UseThisStr.Replace(",Remarks", "")
+            UseThisStr = UseThisStr.Replace(",'[Remarks]'", "")
+        Else
+            Remarks = "DeleteThis"
+            UseThisStr = UseThisStr.Replace("[Remarks]", Remarks)
+        End If
+
+        rowsAffected = SUT1.ExecuteNonQuery(UseThisStr)
+        Assert.IsTrue(rowsAffected = 1)
+
+        'delete
+        UseThisStr = mConfig1.Queries(15)
+        rowsAffected = SUT1.ExecuteNonQuery(UseThisStr)
+        Assert.IsTrue(rowsAffected > 1)
+        SUT1.Disconnect()
+    End Sub
     <TestMethod()> Public Sub ParallelUpload_PopulateQueue_DtTable()
         Dim rowsAffected As Long
         SUT1.Connect()
